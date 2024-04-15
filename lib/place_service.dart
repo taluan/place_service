d184346bf7d6b3391e01e018ba4310fa2b279ff2
taluan <taluan86@gmail.com>
2024-04-15 10:04:20 +0700

    update improt

--- lib/place_service.dart
+++ lib/place_service.dart
@@ -1,5 +1,15 @@
 library place_service;
 
+export 'google_api_service/autocomplete/autocomplete_prediction.dart';
+export 'google_api_service/models/component.dart';
+export 'google_api_service/models/lat_lon.dart';
+export 'google_api_service/details/details.dart';
+export 'google_api_service/directions/directions.dart';
+export 'google_api_service/geocoding/geocoding.dart';
+export 'google_api_service/directions/directions.response.dart';
+export 'google_api_service/geocoding/geocoding_result.dart';
+export 'google_api_service/google_place.dart';
+
 export 'goong_service/goong_service.dart';
 export 'goong_service/places/goong_place_result.dart';
 export 'goong_service/places/goong_places_service.dart';
